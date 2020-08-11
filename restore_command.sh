c2ed1ead1733838f6b0a34644c675bc180d3d0e6
Emamul Andalib <emamulandalib@users.noreply.github.com>
2020-08-11 11:56:44 +0600

    Update restore_command.sh

--- restore_command.sh
+++ restore_command.sh
@@ -3,17 +3,15 @@
 user='user'
 password='password'
 host=localhost
+tables=("table1" "table2")
+key_space='keyspace'
 
 restore_from_csv() {
-    key_space=$1
-    tables=$2[@]
-    tables=("${!tables}")
-
     echo "Creating Keyspace..."
     cqlsh -u $user -p $password -e "DROP KEYSPACE $key_space"
     cqlsh -u $user -p $password -f $key_space/schema.cql
 
-    for table in "${tables[@]}"; do
+    for table in ${tables[@]}; do
         echo "Processing KeySpace: $key_space, Table: $table"
         cqlsh -u $user -p $password -e "COPY $key_space.$table FROM '$key_space/$table.csv' WITH HEADER = true AND NULL = ' '"
         echo "Import successful. KeySpace: $key_space, Table: $table"
@@ -21,6 +19,4 @@ restore_from_csv() {
     done
 }
 
-tables=("table1" "table2")
-key_space='keyspace'
-backup_to_csv "$key_space" tables
+backup_to_csv
